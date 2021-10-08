c335468c97abe2feb056c5d6b8bf147fd054bfc8
Tantchamin <tantchamin@gmail.com>
2021-10-08 23:57:33 +0700

    HW1-ImproveCode and HW2- RegisterActivity

---
+++ 054_HW1_ReCode.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+enum Menu
+{
+    RegisterNewStudent = 1,
+    RegisterNewTeacher,
+    GetListPersons
+}
+
+namespace StuTeache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrepareList.PreparePersonListWhenProgramIsLoad();
+            PrintMenuScreen();
+        }
+
+        static void PrintMenuScreen()
+        {
+            Console.Clear();
+            ShowHeader.PrintHeader();
+            ShowHeader.PrintListMenu();
+            InputMenuFromKeyboard();
+        }
+
+        static void InputMenuFromKeyboard()
+        {
+            Console.Write("Please Select Menu: ");
+            Menu menu = (Menu)(int.Parse(Console.ReadLine()));
+
+            PresentMenu(menu);
+        }
+
+        static void PresentMenu(Menu menu)
+        {
+            if (menu == Menu.RegisterNewStudent)
+            {
+                ShowInputRegisterNewStudentScreen();
+            }
+            else if (menu == Menu.RegisterNewTeacher)
+            {
+                ShowInputRegisterNewTeacherScreen();
+            }
+            else if (menu == Menu.GetListPersons)
+            {
+                ShowGetListPersonScreen();
+            }
+            else
+            {
+                ShowMessageInputMenuIsInCorrect();
+            }
+        }
+
+        static void ShowInputRegisterNewStudentScreen()
+        {
+            Console.Clear();
+            ShowHeader.PrintHeaderRegisterStudent();
+
+            int totalStudent = InputInformation.TotalNewStudents();
+            InputNewStudentFromKeyboard(totalStudent);
+        }
+
+        static void ShowInputRegisterNewTeacherScreen()
+        {
+            Console.Clear();
+            ShowHeader.PrintHeaderRegisterTeacher();
+
+            int totalTeacher = InputInformation.TotalNewTeacher();
+            InputNewTeacherFromKeyboard(totalTeacher);
+        }
+
+        static void ShowGetListPersonScreen()
+        {
+            Console.Clear();
+            PrepareList.personList.FetchPersonsList();
+            InputExitFromKeyboard();
+        }
+
+        static void InputExitFromKeyboard()
+        {
+            string text = "";
+            while (text != "exit")
+            {
+                Console.WriteLine("Input: ");
+                text = Console.ReadLine();
+            }
+
+            Console.Clear();
+            PrintMenuScreen();
+        }
+
+        static void InputNewTeacherFromKeyboard(int totalTeacher)
+        {
+            for (int i = 0; i < totalTeacher; i++)
+            {
+                Console.Clear();
+                ShowHeader.PrintHeaderRegisterTeacher();
+
+                Teacher teacher = CreateNewPerson.CreateNewTeacher();
+                PrepareList.personList.AddNewPerson(teacher);
+            }
+
+            Console.Clear();
+            PrintMenuScreen();
+        }
+
+        static void InputNewStudentFromKeyboard(int totalStudent)
+        {
+            for (int i = 0; i < totalStudent; i++)
+            {
+                Console.Clear();
+                ShowHeader.PrintHeaderRegisterStudent();
+
+                Student student = CreateNewPerson.CreateNewStudent();
+                PrepareList.personList.AddNewPerson(student);
+            }
+
+            Console.Clear();
+            PrintMenuScreen();
+        }
+
+        static void ShowMessageInputMenuIsInCorrect()
+        {
+            Console.Clear();
+            Console.WriteLine("Menu Incorrect Please try again.");
+            InputMenuFromKeyboard();
+        }
+    }
+
+    class ShowHeader
+    {
+        public static void PrintHeader()
+        {
+            Console.WriteLine("Welcome to registration new user school application.");
+            Console.WriteLine("----------------------------------------------------");
+        }
+
+        public static void PrintListMenu()
+        {
+            Console.WriteLine("1. Register new student.");
+            Console.WriteLine("2. Register new Teacher.");
+            Console.WriteLine("3. Get List Persons.");
+        }
+
+        public static void PrintHeaderRegisterStudent()
+        {
+            Console.WriteLine("Register new student.");
+            Console.WriteLine("---------------------");
+        }
+
+        public static void PrintHeaderRegisterTeacher()
+        {
+            Console.WriteLine("Register new teacher.");
+            Console.WriteLine("---------------------");
+        }
+    }
+
+    class CreateNewPerson
+    {
+        public static Student CreateNewStudent()
+        {
+            return new Student(InputInformation.InputName(),
+             InputInformation.InputAddress(),
+             InputInformation.InputCitizenID(),
+             InputInformation.InputStudentID());
+        }
+
+        public static Teacher CreateNewTeacher()
+        {
+            return new Teacher(InputInformation.InputName(),
+            InputInformation.InputAddress(),
+            InputInformation.InputCitizenID(),
+            InputInformation.InputEmployeeID());
+        }
+    }
+
+    class InputInformation
+    {
+        public static string InputName()
+        {
+            Console.Write("Name: ");
+            return Console.ReadLine();
+        }
+
+        public static string InputStudentID()
+        {
+            Console.Write("Student ID: ");
+            return Console.ReadLine();
+        }
+
+        public static string InputAddress()
+        {
+            Console.Write("Address: ");
+            return Console.ReadLine();
+        }
+
+        public static string InputCitizenID()
+        {
+            Console.Write("Citizen ID: ");
+            return Console.ReadLine();
+        }
+
+        public static string InputEmployeeID()
+        {
+            Console.Write("Employee ID: ");
+            return Console.ReadLine();
+        }
+
+        public static int TotalNewStudents()
+        {
+            Console.Write("Input Total new Student: ");
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static int TotalNewTeacher()
+        {
+            Console.Write("Input Total new Teacher: ");
+            return int.Parse(Console.ReadLine());
+        }
+    }
+
+    class PrepareList
+    {
+        public static PersonList personList;
+
+        public static void PreparePersonListWhenProgramIsLoad()
+        {
+            PrepareList.personList = new PersonList();
+        }
+    }
+
+    class Person
+    {
+        protected string name;
+        protected string address;
+        protected string citizenID;
+
+        public Person(string name, string address, string citizenID)
+        {
+            this.name = name;
+            this.address = address;
+            this.citizenID = citizenID;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+    }
+
+    class PersonList
+    {
+        private List<Person> personList;
+
+        public PersonList()
+        {
+            this.personList = new List<Person>();
+        }
+
+        public void AddNewPerson(Person person)
+        {
+            this.personList.Add(person);
+        }
+
+        public void FetchPersonsList()
+        {
+            Console.WriteLine("List Persons");
+            Console.WriteLine("---------------------");
+            foreach (Person person in this.personList)
+            {
+                if (person is Student)
+                {
+                    Console.WriteLine("Name: {0} \nType: Student \n", person.GetName());
+                }
+                else if (person is Teacher)
+                {
+                    Console.WriteLine("Name: {0} \nType: Teacher \n", person.GetName());
+                }
+            }
+        }
+    }
+
+    class Student : Person
+    {
+        private string studentID;
+
+        public Student(string name, string address, string citizenID, string studentID)
+        : base(name, address, citizenID)
+        {
+            this.studentID = studentID;
+        }
+
+    }
+
+    class Teacher : Person
+    {
+        private string employeeID;
+
+        public Teacher(string name, string address, string citizenID, string employeeID)
+        : base(name, address, citizenID)
+        {
+            this.employeeID = employeeID;
+        }
+
+    }
+
+}
---
+++ 054_HW2_Register.cs
@@ -0,0 +1,569 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _054_HW_Register
+{
+    enum Menu
+    {
+        Login = 1,
+        Register,
+        Exit
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrepareList.PreparePersonList();
+            PrepareList.PrepareActivityList();
+            MenuScreen();
+
+        }
+
+        static void MenuScreenHeader()
+        {
+            Console.WriteLine("Welcome To Acitivity Register Site");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("1. Login");
+            Console.WriteLine("2. Register ID");
+            Console.WriteLine("3. Exit");
+        }
+
+        static void MenuScreen()
+        {
+            MenuScreenHeader();
+            MenuScreenInput();
+        }
+
+        static void MenuScreenInput()
+        {
+            Console.Write("Please Select Menu: ");
+            Menu menu = (Menu)int.Parse(Console.ReadLine());
+
+            MenuScreenSelectCheck(menu);
+        }
+
+        static void MenuScreenSelectCheck(Menu menu)
+        {
+            if (menu == Menu.Login)
+            {
+                Console.Clear();
+                LoginMenu();
+            }
+            else if (menu == Menu.Register)
+            {
+                Console.Clear();
+                RegisterMenu();
+            }
+            else if (menu == Menu.Exit)
+            {
+                
+            }
+            else
+            {
+                Console.Clear();
+                MenuScreen();
+            }
+        }
+
+        static void LoginMenu()
+        {
+            LoginMenuHeader();
+            LoginMenuInput();
+        }
+
+        static void LoginMenuHeader()
+        {
+            Console.WriteLine("Login Menu");
+            Console.WriteLine("----------");
+        }
+
+        static void LoginMenuInput()
+        {
+            string username = InputInformation.InputName();
+            string password = InputInformation.InputPassword();
+
+            CheckUsernamePassword(username, password);
+        }
+
+        static void CheckUsernamePassword(string username, string password)
+        {
+            int i = 0;
+            int counter = 0;
+            foreach (Person person in PrepareList.personList.personList)
+            {
+                if (username == PrepareList.personList.personList[i].Name && password == PrepareList.personList.personList[i].Password)
+                {
+                    if (PrepareList.personList.personList[i].Type == 1)
+                    {
+                        string studentID = PrepareList.personList.personList[i].PersonID();
+
+                        Console.Clear();
+                        StudentMenu(username, studentID);
+                        counter++;
+                    }
+                    else if (PrepareList.personList.personList[i].Type == 2)
+                    {
+                        string employeeID = PrepareList.personList.personList[i].PersonID();
+
+                        Console.Clear();
+                        EmployeeMenu(username, employeeID);
+                        counter++;
+                    }
+                }
+                
+                i++;
+            }
+
+            if (counter < 1)
+            {
+                Console.Clear();
+                MenuScreen();
+            }
+        }
+
+        static void StudentMenu(string name, string ID)
+        {
+            Console.Clear();
+            StudentMainMenu.StudentMenuMain(name, ID);
+            int menu = RegisterAcitvity.RegisterMenuInputMenu();
+
+            if (menu == 1)
+            {
+                Console.Clear();
+                StudentMainMenu.StudentMenuMain(name, ID);
+                int activity = RegisterAcitvity.ChooseActivity();
+                RegisterAcitvity.ActivityRegister(name, ID, activity);
+
+                Console.Clear();
+                StudentMenu(name, ID);
+
+            }
+            else if (menu == 2)
+            {
+                Console.Clear();
+                StudentMainMenu.StudentMenuMain(name, ID);
+                PrepareList.acitivityList.ShowRegisterActivity();
+            }
+            
+        }
+
+        static void EmployeeMenu(string name, string ID)
+        {
+            Console.Clear();
+            EmployeeMainMenu.EmployeeMenuMain(name, ID);
+            int menu = RegisterAcitvity.RegisterMenuInputMenu();
+
+            if (menu == 1)
+            {
+                Console.Clear();
+                EmployeeMainMenu.EmployeeMenuMain(name, ID);
+                int activity = RegisterAcitvity.ChooseActivity();
+                RegisterAcitvity.ActivityRegister(name, ID, activity);
+
+                Console.Clear();
+                EmployeeMenu(name, ID);
+
+            }
+            else if (menu == 2)
+            {
+                Console.Clear();
+                EmployeeMainMenu.EmployeeMenuMain(name, ID);
+                PrepareList.acitivityList.ShowRegisterActivity();
+            }
+        }
+
+        static void RegisterMenu()
+        {
+            RegisterMenuHeader();
+            RegisterMenuInput();
+        }
+
+        static void RegisterMenuHeader()
+        {
+            Console.WriteLine("Register Menu");
+            Console.WriteLine("-------------");
+        }
+
+        static void RegisterMenuInput()
+        {
+            string name = InputInformation.InputName();
+            string password = InputInformation.InputPassword();
+            int type = InputInformation.InputType();
+
+            if (type == 1)
+            {
+                string studentID = InputInformation.InputStudentID();
+
+                Student student = CreatePerson.CreateNewStudent(name, password, type, studentID);
+                PrepareList.personList.AddNewPerson(student);
+                student.PersonID();
+
+                Console.Clear();
+                MenuScreen();
+            }
+            else if (type == 2)
+            {
+                string employeeID = InputInformation.InputEmployeeID();
+
+                Employee employee = CreatePerson.CreateNewEmployee(name, password, type, employeeID);
+                PrepareList.personList.AddNewPerson(employee);
+                employee.PersonID();
+
+                Console.Clear();
+                MenuScreen();
+            }
+        }
+    }
+
+    class StudentMainMenu
+    {
+        public static void StudentMenuMain(string name, string ID)
+        {
+            StudentMenuHeader();
+            StudentMenuShowNameID(name, ID);
+
+        }
+
+        public static void StudentMenuHeader()
+        {
+            Console.WriteLine("Student Menu");
+            Console.WriteLine("------------");
+        }
+
+        public static void StudentMenuShowNameID(string name, string ID)
+        {
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("Student ID: {0}", ID);
+            Console.WriteLine("------------");
+        }
+    }
+
+    class RegisterAcitvity
+    {
+        public static int RegisterMenuInputMenu()
+        {
+            Console.WriteLine("1. Register Activity");
+            Console.WriteLine("2. Exit");
+            Console.Write("Input Menu: ");
+            int menu = int.Parse(Console.ReadLine());
+
+            while (menu < 1 || menu > 2)
+            {
+                Console.Write("Input Menu: ");
+                menu = int.Parse(Console.ReadLine());
+            }
+
+            return menu;
+        }
+
+        static void ActivityToChoose()
+        {
+            Console.WriteLine("1. Media Battleground");
+            Console.WriteLine("2. Yu-Gi-Oh Battle Royale");
+            Console.WriteLine("3. Dream Colored Patissier");
+        }
+
+        public static int ChooseActivity()
+        {
+            Console.WriteLine("Input The Number Of Activity To Register");
+            ActivityToChoose();
+            Console.Write("Input Number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            ChooseActivityCheck(number);
+
+            return number;
+        }
+
+        static void ChooseActivityCheck(int number)
+        {
+            while (number < 1 || number > 3)
+            {
+                Console.Write("Number Incorrect Input New Number: ");
+                number = int.Parse(Console.ReadLine());
+            }
+        }
+
+        public static void ActivityRegister(string name, string ID, int number)
+        {
+            if (number == 1)
+            {
+                MediaBattleground mediaBattleground = CreateRegisterActivity.RegisterMedia(name, ID);
+                PrepareList.acitivityList.RegisterActivity(mediaBattleground);
+
+            }
+            else if (number == 2)
+            {
+                YuGiOhBattleRoyal yugiohBattleRoyal = CreateRegisterActivity.RegisterYGO(name, ID);
+                PrepareList.acitivityList.RegisterActivity(yugiohBattleRoyal);
+
+            }
+            else if (number == 3)
+            {
+                DreamColoredPatissier dreamcolorPatissier = CreateRegisterActivity.RegisterPatissier(name, ID);
+                PrepareList.acitivityList.RegisterActivity(dreamcolorPatissier);
+
+            }
+        }
+    }
+
+    class EmployeeMainMenu
+    {
+        public static void EmployeeMenuMain(string name, string ID)
+        {
+            EmployeeMenuHeader();
+            EmployeeMenuShowNameID(name, ID);
+        }
+        static void EmployeeMenuHeader()
+        {
+            Console.WriteLine("Employee Menu");
+            Console.WriteLine("-------------");
+        }
+
+        static void EmployeeMenuShowNameID(string name, string ID)
+        {
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("Employee ID: {0}", ID);
+            Console.WriteLine("-------------");
+        }
+    }
+
+    class InputInformation
+    {
+        public static string InputName()
+        {
+            Console.Write("Input Name: ");
+            return Console.ReadLine();
+        }
+
+        public static string InputPassword()
+        {
+            Console.Write("Input Password: ");
+            return Console.ReadLine();
+        }
+
+        public static int InputType()
+        {
+            Console.Write("Input Type 1. Student, 2. Employee: ");
+            int type = int.Parse(Console.ReadLine());
+            while (type != 1 && type != 2)
+            {
+                Console.WriteLine("Wrong Number Please Input Again");
+                type = int.Parse(Console.ReadLine());
+            }
+
+            return type;
+        }
+
+        public static string InputStudentID()
+        {
+            Console.Write("Input Your StudentID: ");
+            return Console.ReadLine();
+        }
+
+        public static string InputEmployeeID()
+        {
+            Console.Write("Input Your EmployeeID: ");
+            return Console.ReadLine();
+        }
+    }
+
+    class CreatePerson
+    {
+        public static Student CreateNewStudent(string name, string password, int type, string studentID)
+        {
+            return new Student(name, password, type, studentID);
+        }
+
+        public static Employee CreateNewEmployee(string name, string password, int type, string employeeID)
+        {
+            return new Employee(name, password, type, employeeID);
+        }
+
+    }
+
+    class CreateRegisterActivity
+    {
+        public static MediaBattleground RegisterMedia(string name, string ID)
+        {
+            return new MediaBattleground(name, ID);
+        }
+
+        public static YuGiOhBattleRoyal RegisterYGO(string name, string ID)
+        {
+            return new YuGiOhBattleRoyal(name, ID);
+        }
+
+        public static DreamColoredPatissier RegisterPatissier(string name, string ID)
+        {
+            return new DreamColoredPatissier(name, ID);
+        }
+    }
+
+    class PrepareList
+    {
+        public static PersonList personList;
+
+        public static void PreparePersonList()
+        {
+            personList = new PersonList();
+        }
+
+        public static AcitivityList acitivityList;
+
+        public static void PrepareActivityList()
+        {
+            acitivityList = new AcitivityList();
+        }
+
+    }
+
+    class Person
+    {
+        public string Name;
+        public string Password;
+        public int Type;
+        public virtual string PersonID()
+        {
+            return "0";
+        }
+
+        public Person(string name, string password, int type)
+        {
+            this.Name = name;
+            this.Password = password;
+            this.Type = type;
+        }
+    }
+
+    class Student: Person
+    {
+        public string StudentID;
+
+        public Student(string name, string password, int type, string studentID) : base(name, password, type)
+        {
+            this.StudentID = studentID;
+        }
+
+        public override string PersonID()
+        {
+            return this.StudentID;
+        }
+    }
+
+    class Employee: Person
+    {
+        public string EmployeeID;
+
+        public Employee(string name, string password, int type, string employeeID) : base(name, password, type)
+        {
+            this.EmployeeID = employeeID;
+        }
+
+        public override string PersonID()
+        {
+            return this.EmployeeID;
+        }
+    }
+
+    class PersonList
+    {
+        public List<Person> personList;
+        public PersonList()
+        {
+            this.personList = new List<Person>();
+        }
+
+        public void AddNewPerson(Person newPerson)
+        {
+            this.personList.Add(newPerson);
+        }
+    }
+
+    class Activity
+    {
+        public string Username;
+        public string ID;
+
+        public Activity(string username, string id)
+        {
+            this.Username = username;
+            this.ID = id;
+        }
+
+        public string GetName()
+        {
+            return this.Username;
+        }
+
+        public string GetID()
+        {
+            return this.ID;
+        }
+    }
+
+    class MediaBattleground: Activity
+    {
+        public MediaBattleground(string username, string id): base(username, id) { }
+
+    }
+
+    class YuGiOhBattleRoyal: Activity
+    {
+        public YuGiOhBattleRoyal(string username, string id) : base(username, id) { }
+
+    }
+
+    class DreamColoredPatissier: Activity
+    {
+        public DreamColoredPatissier(string username, string id) : base(username, id) { }
+
+    }
+
+    class AcitivityList
+    {
+        public List<Activity> activityList;
+
+        public AcitivityList()
+        {
+            this.activityList = new List<Activity>();
+        }
+
+        public void RegisterActivity(Activity newActivity)
+        {
+            this.activityList.Add(newActivity);
+        }
+
+        public void ShowRegisterActivity()
+        {
+            int counter = 0;
+            Console.WriteLine("Registered Acitivity");
+            Console.WriteLine("-------------------");
+            foreach (Activity activity in this.activityList)
+            {
+                if(activity is MediaBattleground)
+                {
+                    Console.WriteLine("Media Battleground - Name: {0} ID: {1}", activity.GetName(), activity.GetID());
+                    counter++;
+                }
+                else if (activity is YuGiOhBattleRoyal)
+                {
+                    Console.WriteLine("Yu-Gi-Oh Battle Royale - Name: {0} ID: {1}", activity.GetName(), activity.GetID());
+                    counter++;
+                }
+                else if (activity is DreamColoredPatissier)
+                {
+                    Console.WriteLine("Dream Colored Patissier - Name: {0} ID: {1}", activity.GetName(), activity.GetID());
+                    counter++;
+                }
+            }
+            if (counter == 0)
+            {
+                Console.WriteLine("None Activity Has Registered");
+            }
+        }
+
+    }
+
+}
